b9ef4126aa9b54c0e39aea90bba8a1d511d04f2d
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-08 21:34:28 -0500

    Add images to home.jsx.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,34 @@
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        Boost Your Make Up Collection And Your Bottom Line With Organized
+        Inventory Management
+      </h1>
+      <section>
+        <h2>Add New Inventory</h2>
+        <img
+          class="new-inventory"
+          src="single-resource-front-end/src/assets/new-inventory.jpeg"
+          alt=""
+        />
+      </section>
+      <section>
+        <h2>Track Your Inventory</h2>
+        <img
+          class="track-inventory"
+          src="single-resource-front-end/src/assets/track-inventory.jpeg"
+          alt=""
+        />
+      </section>
+      <section>
+        <h2>Manage Your Inventory</h2>
+        <img
+          class="manage-inventory"
+          src="single-resource-front-end/src/assets/manage-inventory.avif"
+          alt=""
+        />
+      </section>
+    </div>
+  );
 }
