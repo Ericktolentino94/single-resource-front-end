c7463a859c644781d0b0c23c9bda8e2593e2e9d4
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-11-15 20:19:52 -0500

    Update Home.css.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,8 @@
-
 import newInventoryImage from "../assets/new-inventory.jpeg";
 import trackInventoryImage from "../assets/track-inventory.jpeg";
 import manageInventoryImage from "../assets/manage-inventory.avif";
 
-
-import "./Home.css"
+import "./Home.css";
 
 function Home() {
   return (
@@ -13,30 +11,32 @@ function Home() {
         Boost Your Make Up Collection And Your Bottom Line With Organized
         Inventory Management
       </h1>
-      <section>
-        <h2>Add New Inventory</h2>
-        <img
-          className="new-inventory"
-          src={newInventoryImage}
-          alt="cartoon checks new inventory"
-        />
-      </section>
-      <section>
-        <h2>Track Your Inventory</h2>
-        <img
-          className="track-inventory"
-          src={trackInventoryImage}
-          alt="cartoon checks boxes for inventory"
-        />
-      </section>
-      <section>
-        <h2>Manage Your Inventory</h2>
-        <img
-          className="manage-inventory"
-          src={manageInventoryImage}
-          alt="cartoon warehouse worker checks inventory"
-        />
-      </section>
+      <div className="home-inventory">
+        <section>
+          <h2>Add New Inventory</h2>
+          <img
+            className="new-inventory"
+            src={newInventoryImage}
+            alt="cartoon checks new inventory"
+          />
+        </section>
+        <section>
+          <h2>Track Your Inventory</h2>
+          <img
+            className="track-inventory"
+            src={trackInventoryImage}
+            alt="cartoon checks boxes for inventory"
+          />
+        </section>
+        <section>
+          <h2>Manage Your Inventory</h2>
+          <img
+            className="manage-inventory"
+            src={manageInventoryImage}
+            alt="cartoon warehouse worker checks inventory"
+          />
+        </section>
+      </div>
     </div>
   );
 }
